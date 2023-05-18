ac081de82f9f108718d1d1d152b8fadd02e1bbc6
Dhiraj-114 <105421318+Dhiraj-114@users.noreply.github.com>
2023-05-18 12:55:36 +0530

    Add files via upload

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,17 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_label_HODING_LOCATION_2_eval_expression(context) {
-    // "XX" || '\n' || "YY"
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return ((feature.properties['XX']  + '\n') + feature.properties['YY'] );
-    } else {
-        return ((feature['XX']  + '\n') + feature['YY'] );
-    }
-}
